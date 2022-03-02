cc5de28bb341b40bc09d1fc8bff93d87446457c8
Tomer Eyal <tomere@moveo.co.il>
2022-03-02 12:17:34 +0200

    added mock-data,images and icons to respective files

---
+++ src/interfaces/dish.interface.ts
@@ -0,0 +1,10 @@
+export interface IDish {
+  name: string;
+  descr: string;
+  url: string;
+  restaurant: string;
+  price: number;
+  logo?: string;
+  url2x?: string;
+  url3x?: string;
+}
---
+++ src/interfaces/route.interface.ts
@@ -0,0 +1,3 @@
+export interface IRoute {
+  label: string;
+}
