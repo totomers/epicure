8030856409865f543b8be8517809c5373fd5a3d0
Tomer Eyal <tomere@moveo.co.il>
2022-03-06 11:15:14 +0200

    still need to fix

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -13,16 +13,27 @@ function Navbar() {
       <div className="navbar">
         <div className="left-navbar">
           <Logo></Logo>
-          <Routes></Routes>
+          <div className="hide-for-mobile">
+            <Routes></Routes>
+          </div>
+        </div>
+        <div className="absolute hide-for-non-mobile">
+          <Logo></Logo>
         </div>
         <div className="right-navbar">
           <Input
             iconUrl="/icons/search-icon.svg"
             iconSize={22}
             iconDirection={"right"}
-            className="navbar-input"
+            className="navbar-input hide-for-mobile"
             placeHolder="Search for restaurant cuisine, chef"
           ></Input>
+          <Icon
+            url="/icons/search-icon.svg"
+            size={22}
+            customClass={"hide-for-non-mobile"}
+          ></Icon>
+
           <Icon url="/icons/user-icon.svg" size={22}></Icon>
           <Icon url="/icons/bag-icon.svg" size={22}></Icon>
         </div>
